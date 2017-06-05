3b18861d53372f949c99e8b82d57c3d5d4be317f
Roman Kukharuk <rkukharuk@gmail.com>
2017-06-04 21:28:16 -0700

    New level

--- src/com/withgoogle/foobar/Main.java
+++ src/com/withgoogle/foobar/Main.java
@@ -1,10 +1,13 @@
 package com.withgoogle.foobar;
 
-import com.withgoogle.foobar.level2_1.Answer;
+//import com.withgoogle.foobar.level2_1.Answer;
+import com.withgoogle.foobar.level2_2.Answer;
 
 public class Main {
 
     public static void main(String[] args) {
+        /*
+        //Level 2_1
         Answer ans = new Answer();
         System.out.println("rez: " + ans.answer(1,1).equals("1"));
         System.out.println("rez: " + ans.answer(1,2).equals("2"));
@@ -18,6 +21,47 @@ public class Main {
         System.out.println("rez: " + ans.answer(2,3).equals("8"));
         System.out.println("rez: " + ans.answer(3,2).equals("9"));
         System.out.println("rez: " + ans.answer(99999,99999).equals("19999400005"));
+         */
+
+        //Level 2_2
+        Answer ans = new Answer();
+        System.out.println("\n");
+        for (int x: ans.answer(new int[]{4, 30, 50})){
+
+            System.out.print(x + " ");
+        }
+        System.out.println("\n");
+        for (int x: ans.answer(new int[]{4, 17, 50})) {
+            System.out.print(x + " ");
+        }
+        System.out.println("\n");
+        for (int x: ans.answer(new int[]{4, 30, 50, 80, 100})){
+            System.out.print(x + " ");
+        }
+        System.out.println("\n");
+        for (int x: ans.answer(new int[]{4, 30, 50})){
+            System.out.print(x + " ");
+        }
+        System.out.println("\n");
+        for (int x: ans.answer(new int[]{4, 17})) {
+            System.out.print(x + " ");
+        }
+        System.out.println("\n");
+        for (int x: ans.answer(new int[]{18, 40, 100, 150})) {
+            System.out.print(x + " ");
+        }
+        System.out.println("\n");
+        for (int x: ans.answer(new int[]{20, 40, 60, 100})) {
+            System.out.print(x + " ");
+        }
+        System.out.println("\n");
+        for (int x: ans.answer(new int[]{20, 40, 60, 90})) {
+            System.out.print(x + " ");
+        }
+        System.out.println("\n");
+        for (int x: ans.answer(new int[]{20, 40, 60, 80})) {
+            System.out.print(x + " ");
+        }
     }
 }
 
---
+++ src/com/withgoogle/foobar/level2_2/Answer.java
@@ -0,0 +1,65 @@
+package com.withgoogle.foobar.level2_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Answer {
+
+    public static int[] answer(int[] pegs) {
+
+        int firstGearSize = lastGearSize(pegs) * 2;
+        int divisor = 1;
+
+        if ((pegs.length % 2 == 0)) {
+            if (firstGearSize % 3 == 0) {
+                firstGearSize /= 3;
+            } else {
+                divisor = 3;
+            }
+        }
+        return allGearsFit(firstGearSize, divisor, pegs);
+    }
+
+    private static int[] allGearsFit(int firstGearSize, int divisor, int[] pegs) {
+        //find incorrect vallues
+        float prevR = ((float) firstGearSize) / ((float) divisor);
+        for (int i = 0; i < pegs.length - 2; i++) {
+            int width = pegs[i + 1] - pegs[i];
+            if (prevR < 0 || prevR > (width - 1)) return new int[]{-1, -1};
+            prevR = width - prevR;
+        }
+        if (firstGearSize > 1){
+            return new int[]{firstGearSize, divisor};
+        }
+        else return new int[]{-1,-1};
+
+    }
+
+    //find y
+    public static int lastGearSize(int[] pegs){
+        int sign;
+        if (pegs.length % 2 == 0){
+            sign = 1;
+        } else  sign = -1;
+
+        List<Integer> listDist = new ArrayList<>();
+        for (int i = 0; i < pegs.length - 1; i++) {
+            listDist.add(pegs[i + 1] - pegs[i]);
+        }
+        int y= 0;
+        //odd    y = (d(1) - d(2) + d(3) - d(4))/1  ## [general form: sign = -1, divisor = 1]
+        //even    y = (d(1) - d(2) + d(3))/3   ## [general form: sign = +1, divisor = 3]
+        int c = 1;
+        for (int i = 0; i<listDist.size(); i++) {
+            if (i == listDist.size() - 1) {
+                y = y + (sign) * listDist.get(i);
+            } else if (c % 2 == 0) {
+                y -= listDist.get(i);
+            } else y += listDist.get(i);
+            c++;
+        }
+        return y;
+    }
+}
+
+
