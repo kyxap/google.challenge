3383a787e97f2c4b33cc091fbc9347f667614bad
Roman Kukharuk <rkukharuk@gmail.com>
2017-06-04 21:56:36 -0700

    New level

--- src/com/withgoogle/foobar/Main.java
+++ src/com/withgoogle/foobar/Main.java
@@ -1,7 +1,8 @@
 package com.withgoogle.foobar;
 
 //import com.withgoogle.foobar.level2_1.Answer;
-import com.withgoogle.foobar.level2_2.Answer;
+//import com.withgoogle.foobar.level2_2.Answer;
+import com.withgoogle.foobar.level2_2_1.Answer;
 
 public class Main {
 
@@ -23,6 +24,7 @@ public class Main {
         System.out.println("rez: " + ans.answer(99999,99999).equals("19999400005"));
          */
 
+        /*
         //Level 2_2
         Answer ans = new Answer();
         System.out.println("\n");
@@ -62,6 +64,32 @@ public class Main {
         for (int x: ans.answer(new int[]{20, 40, 60, 80})) {
             System.out.print(x + " ");
         }
+        */
+
+        //Level 2_2 (bug)
+        Answer a = new Answer();
+        System.out.println(a.answer(new int[]{2, 0, 2, 2, 0}).equals("8"));
+        System.out.println("=================================");
+        System.out.println(a.answer(new int[]{-2, -3, 4, -5}).equals("60"));
+
+        System.out.println("=================================");
+        System.out.println(a.answer(new int[]{0,-1,-2,-3,5}).equals("30"));
+
+        System.out.println("=================================");
+        System.out.println(a.answer(new int[]{0,2,2,3,5}).equals("60"));
+
+        System.out.println("=================================");
+        System.out.println(a.answer(new int[]{0,0,0,0,-5}).equals("0"));
+
+        System.out.println("=================================");
+        System.out.println(a.answer(new int[]{1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000}).equals("1000000000000000000000000000000000000000000000000000000000"));
+
+        System.out.println("=================================");
+        System.out.println(a.answer(new int[]{2,2,2,-2,-5}).equals("40"));
+
+
+        System.out.println("=================================");
+        System.out.println(a.answer(new int[]{0,0,0,0,5}).equals("5"));
     }
 }
 
